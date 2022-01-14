cd52aff673a6a3332ae8e021bc1f417a1ae9880e
rd4cake <sharky7511@gmail.com>
2022-01-13 19:56:18 -0500

    changed origin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000/", credentials: "true" }));
+app.use(cors({ origin: "http://localhost:3000", credentials: "true" }));
 
 app.set("trust proxy",1)
 
